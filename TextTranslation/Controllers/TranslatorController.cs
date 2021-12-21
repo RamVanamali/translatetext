0883e21c24805ed03090833f7d888bf247fcf14f
RamVanamali <96421070+RamVanamali@users.noreply.github.com>
2021-12-21 23:37:59 +0530

    Add files via upload

---
+++ TextTranslation/Controllers/TranslatorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace TextTranslation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TranslatorController : ControllerBase
+    {
+        /// Translate the given text in to selected language.  
+        /// </summary>  
+        /// <param name="text">The text is given for translation</param> 
+        /// <returns></returns>  
+      
+       [HttpPost]
+        public async Task<string> Translate([FromBody]string text)
+        {
+            System.Object[] body = new System.Object[] { new { Text = text } };
+            var requestBody = JsonConvert.SerializeObject(body);
+            string key = "";
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri("https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=fr");
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+                //request.Headers.Add("Ocp-Apim-Subscription-Region", "fr");
+
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                dynamic result = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(responseBody), Formatting.Indented);
+
+                return result;
+            }
+        }
+    }
+}
